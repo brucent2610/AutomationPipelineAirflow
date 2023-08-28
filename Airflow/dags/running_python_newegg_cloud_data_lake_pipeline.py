21e4865d37f2530db905d3c540050f7eb346b534
Phong Nguyen <phong.nt.26101993@gmail.com>
2023-08-28 16:50:02 +0800

    + Init newegg pipeline

--- Airflow/dags/running_python_newegg_cloud_data_lake_pipeline.py
+++ Airflow/dags/running_python_newegg_cloud_data_lake_pipeline.py
@@ -13,35 +13,38 @@ default_args = {
 }
 
 project_id = 'data-engineer-393307'
-staging_dataset = 'ecommerce_tiki_staging'
-production_dataset = 'ecommerce_tiki'
+staging_dataset = 'ecommerce_newegg_staging'
+production_dataset = 'ecommerce_newegg'
 gs_bucket = 'data-engineer-393307-cloud-data-lake'
 
-with DAG('tiki_cloud_data_lake_pipeline',
+with DAG('newegg_cloud_data_lake_pipeline',
     schedule_interval=timedelta(days=1),
     default_args=default_args) as dag:
 
     start_pipeline = DummyOperator(task_id = 'start_pipeline')
 
     load_products = GoogleCloudStorageToBigQueryOperator(
-        task_id = 'load_tiki_products',
+        task_id = 'load_newegg_products',
         bucket = gs_bucket,
-        source_objects = ['products-2023-08-09-converted.json'],
+        source_objects = ['Products.csv'],
         destination_project_dataset_table = f'{project_id}:{staging_dataset}.products',
-        schema_object = 'schema/schema.json',
         write_disposition='WRITE_TRUNCATE',
-        source_format = 'NEWLINE_DELIMITED_JSON',
-        create_disposition="CREATE_IF_NEEDED"
+        source_format="CSV",
+        skip_leading_rows = 1,
+        max_bad_records=100,
+        autodetect=True,
+        encoding="UTF-8",
+        create_disposition="CREATE_IF_NEEDED",
     )
 
     check_us_cities_demo = BigQueryCheckOperator(
-        task_id = 'check_tiki_products',
+        task_id = 'check_newegg_products',
         use_legacy_sql=False,
         location="asia-northeast1",
         sql = f'SELECT count(*) FROM `{project_id}.{staging_dataset}.products`'
     )
 
-    loaded_data_to_staging = DummyOperator(task_id = 'loaded_tiki_products_to_staging')
+    loaded_data_to_staging = DummyOperator(task_id = 'loaded_newegg_products_to_staging')
 
     end_pipeline = DummyOperator(task_id='end_pipeline')
 
