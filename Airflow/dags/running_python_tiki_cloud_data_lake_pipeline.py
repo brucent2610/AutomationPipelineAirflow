b248836832d16939bc02b22d3714ac1a3c01d2c8
Phong Nguyen <phong.nt.26101993@gmail.com>
2023-08-28 12:20:29 +0800

    + Update tiki_cloud_data_lake_pipeline

---
+++ Airflow/dags/running_python_tiki_cloud_data_lake_pipeline.py
@@ -0,0 +1,53 @@
+from airflow import DAG
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime, timedelta
+from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
+from airflow.contrib.operators.bigquery_check_operator import BigQueryCheckOperator
+
+default_args = {
+    'start_date': datetime.datetime(2023, 8, 25),
+    'owner': 'Phong Nguyen',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5),
+}
+
+project_id = 'data-engineer-393307'
+staging_dataset = 'ecommerce_tiki_staging'
+production_dataset = 'ecommerce_tiki'
+gs_bucket = 'data-engineer-393307-cloud-data-lake'
+
+with DAG('tiki_cloud_data_lake_pipeline',
+    schedule_interval=datetime.timedelta(days=1),
+    default_args=default_dag_args) as dag:
+
+    start_pipeline = DummyOperator(
+        task_id = 'start_pipeline'
+    )
+
+    load_products = GoogleCloudStorageToBigQueryOperator(
+        task_id = 'load_tiki_products',
+        bucket = gs_bucket,
+        source_objects = ['products-2023-08-09-converted.json'],
+        destination_project_dataset_table = f'{project_id}:{staging_dataset}.products',
+        schema_object = 'schema/schema.json',
+        write_disposition='WRITE_TRUNCATE',
+        source_format = 'NEWLINE_DELIMITED_JSON'
+    )
+
+    check_us_cities_demo = BigQueryCheckOperator(
+        task_id = 'check_tiki_produces',
+        use_legacy_sql=False,
+        sql = f'SELECT count(*) FROM `{project_id}.{staging_dataset}.products`'
+    )
+
+    loaded_data_to_staging = DummyOperator(
+        task_id = 'loaded_products_to_staging'
+    )
+
+    end = DummyOperator(
+        task_id='end_pipeline')
+
+    start_pipeline >> load_products >> check_us_cities_demo >> loaded_data_to_staging >> end
+
+    
